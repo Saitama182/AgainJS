844b83fc533a8b36e158e615c2fa77bd00484431
Saitama182 <100026214+Saitama182@users.noreply.github.com>
2023-02-24 21:09:00 +0300

    Массивы

---
+++ js.Pm.Bs.Mst.js
@@ -0,0 +1,76 @@
+/* №1 */
+let num1 = 1;
+let num2 = 2;
+console.log('сумма: ' + (num1 + num2));
+
+/* №2 */
+let a = 1;
+let b = 2;
+console.log(a + b );
+
+/* №3 */
+
+let num = '123';
+let sum = Number(num[0]) + Number(num[1]) + Number(num[2]);
+console.log(sum);
+
+/* №4 */
+
+let num4 = String(123);
+console.log(num4[0]);
+
+/* №5 */
+
+let c = 0;
+console.log(++c);
+
+/* №6 */
+
+let num6 = String(123);
+console.log(num6.length);
+
+/* №7 */
+
+let d = 24 * 60 * 60;
+console.log(d);
+
+/* №8 */
+
+let num8 = 123;
+let str8 = String(num8);
+console.log(str8.length);
+
+/* №9 */
+
+let num9 = 123;
+let str9 = String(num9);
+console.log(str9[str9.length-1]);
+
+/* №10 */
+
+let num10 = 123;
+let str10 = String(num10);
+console.log(str10.length);
+
+/* №11 */
+
+let num11 = 123;
+let str11 = String(num11);
+console.log(str11[str11.length-1]);
+
+/* №12 */
+
+let f = '123';
+let g = '456';
+let s = Number(f) + Number(g);
+
+console.log(s);
+
+/* №13 */
+
+let aaa = Number(1);
+let bbb = Number(2);
+let ccc = Number(3);
+
+console.log(aaa + bbb + ccc);
+
---
+++ js.Pm.Bs.PrmF.js
@@ -0,0 +1,38 @@
+/* №1 */
+
+let r = 12;
+let s = 3.14*(r**2);
+console.log(s);
+
+/* №2 */
+let a = 14;
+let ss = (a**2);
+console.log(ss);
+
+/* №3 */
+
+let aa = 12;
+let bb = 14;
+let sss = aa*bb;
+console.log(sss);
+
+/* №4 */
+
+let aaa = 12;
+let bbb = 14;
+let ssss = (aa+bb)*2;
+console.log(ssss);
+
+/* №5 */
+
+let tc = 123;
+
+let tf = ((5/9)*tc)+32
+console.log(tf.toFixed());
+
+/* №6 */
+
+let tff = 12;
+
+let tcc = (5/9)*(tff-32)
+console.log(tcc.toFixed());
